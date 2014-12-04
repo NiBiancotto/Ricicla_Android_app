a28028fe631bbb8f39370e4262b1a6f8cfe1e71b
n <fj>
2014-12-04 17:45:21 +0100

    Aggiunta mappa

---
+++ Ricicla/src/com/example/ricicla/Maps.java
@@ -0,0 +1,14 @@
+package com.example.ricicla;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Maps extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maps);
+    }
+}
+
